69866fb83c5c926f382c74651787e23823064a92
faina <fainak@trainologic.com>
2017-06-24 10:55:59 +0300

    getUser added

--- src/main/java/rest/UserController.java
+++ src/main/java/rest/UserController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import service.IUserService;
 
 /**
@@ -33,6 +30,13 @@ public class UserController {
         }
 
         return new ResponseEntity<String>(headers, status);
+
+    }
+
+    @RequestMapping(value = "/user/", method = RequestMethod.GET)
+    public User getUser(@RequestParam(required = true) String username)    {
+
+        return userService.getUser(username);
 
     }
 }
